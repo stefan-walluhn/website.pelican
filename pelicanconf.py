6ee49b131ebc8c16ba72e74d620908390ed289ee
Stefan <stefan@neuland.io>
2016-10-15 18:35:21 +0200

    adjust theme settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,11 +34,47 @@ DEFAULT_PAGINATION = 10
 
 RELATIVE_URLS = True
 
-THEME = 'nest'
-NEST_CSS_MINIFY = True
-NEST_SITEMAP_MENU = [('Archives', '/archives.html'),('Tags','/tags.html'), ('Categories','/categories.html')]
 STATIC_PATHS = ['images', 'extra/favicon.ico', 'extra/logo.svg']
 EXTRA_PATH_METADATA = {
     'extra/favicon.ico': {'path': 'favicon.ico'},
     'extra/logo.svg': {'path': 'logo.svg'}
 }
+
+MENUITEMS = [('Archiv', '/archives.html')]
+
+THEME = 'nest'
+NEST_CSS_MINIFY = True
+NEST_SITEMAP_MENU = [('Archiv', '/archives.html'),('Tags','/tags.html'), ('Kategorien','/categories.html')]
+NEST_INDEX_HEAD_TITLE = u'Stefan Walluhn'
+NEST_INDEX_HEADER_TITLE = u'Systemadministrator'
+NEST_INDEX_HEADER_SUBTITLE = u'Gedanken, Erfahrungen, Eindrücke'
+NEST_INDEX_CONTENT_TITLE = u'Letzte Einträge'
+NEST_ARCHIVES_HEAD_TITLE = u'Archiv'
+NEST_ARCHIVES_HEAD_DESCRIPTION = u'Posts Archives'
+NEST_ARCHIVES_HEADER_TITLE = u'Archiv'
+NEST_ARCHIVES_HEADER_SUBTITLE = u'Archiv aller Posts'
+NEST_ARCHIVES_CONTENT_TITLE = u'Archiv'
+NEST_ARTICLE_HEADER_BY = u'Von'
+NEST_ARTICLE_HEADER_MODIFIED = u'verändert'
+NEST_ARTICLE_HEADER_IN = u'veröffentlich in'
+NEST_CATEGORIES_HEAD_TITLE = u'Kategorien'
+NEST_CATEGORIES_HEAD_DESCRIPTION = u'Archives listed by category'
+NEST_CATEGORIES_HEADER_TITLE = u'Kategorien'
+NEST_CATEGORIES_HEADER_SUBTITLE = u'Archives listed by category'
+NEST_CATEGORY_HEAD_TITLE = u'Archiv Kategorie'
+NEST_CATEGORY_HEAD_DESCRIPTION = u'Archiv Kategorie'
+NEST_CATEGORY_HEADER_TITLE = u'Kategorie'
+NEST_CATEGORY_HEADER_SUBTITLE = u'Archiv Kategorie'
+NEST_TAGS_HEAD_TITLE = u'Tags'
+NEST_TAGS_HEAD_DESCRIPTION = u'Tags List'
+NEST_TAGS_HEADER_TITLE = u'Tags'
+NEST_TAGS_HEADER_SUBTITLE = u'Tags List'
+NEST_TAGS_CONTENT_TITLE = u'Tags List'
+NEST_TAGS_CONTENT_LIST = u'tagged'
+NEST_TAG_HEAD_TITLE = u'Archiv Tag'
+NEST_TAG_HEAD_DESCRIPTION = u'Archiv Tag'
+NEST_TAG_HEADER_TITLE = u'Tag'
+NEST_TAG_HEADER_SUBTITLE = u'Archiv Tag'
+NEST_PAGINATION_PREVIOUS = u'Previous'
+NEST_PAGINATION_NEXT = u'Next'
+
